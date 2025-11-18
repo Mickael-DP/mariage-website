e17821846ae364b643c5f02eb9575bfd9df1f46a
Mickael-DP <m.dallepasqualine@gmail.com>
2025-11-18 12:09:45 +0100

    modif footeur & add btn download app photo

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,3 +1,4 @@
+import { MailIcon, PhoneCall, PhoneIcon } from "lucide-react";
 import Container from "./Container";
 
 const Footer = () => {
@@ -7,7 +8,7 @@ const Footer = () => {
         <div className="flex flex-col md:gap-2 md:text-start">
           <h2 className="font-playfair font-bold">Amaury et Angélique</h2>
           <p className="font-marcellus text-text">
-            Notre site de mariage, save the date
+            Nous avons hâte de partager ce moment unique avec vous ! 
           </p>
         </div>
         <div className="flex flex-col md:flex-row gap-6 md:gap-10">
@@ -29,15 +30,16 @@ const Footer = () => {
             </ul>
           </div>
           <div>
-            <h3 className="font-playfair font-bold mb-2">Contact</h3>
+            <h3 className="font-playfair  font-bold mb-2">Contact</h3>
             <ul>
-              <li className="font-marcellus text-text">
-                Adresse email:
-                <a href="mailto: " className="underline" />
+              <li className="font-marcellus flex gap-3 items-center text-text">
+                <MailIcon size={18} />sejalon.angelique@gmail.com 
+                <a href="mailto:sejalon.angelique@gmail.com 
+ " className="underline" />
               </li>
-              <li className="font-marcellus text-text">
-                Téléphone:
-                <a href="tel: " className="underline" />
+              <li className="font-marcellus flex gap-3 items-center text-text">
+                 <PhoneIcon size={18}/>33 6 84 75 54 57
+                <a href="tel:+33684755457" className="underline" />
               </li>
             </ul>
           </div>
@@ -46,7 +48,7 @@ const Footer = () => {
 
       <div className="flex flex-col items-center text-center gap-2 px-4 py-4 text-xs">
         <div className="w-full text-center py-2">
-          © {new Date().getFullYear()} MDP-Développeur Freelance. Tous droits
+          © {new Date().getFullYear()} <a href="https://www.mdallepasqualine.com/" target="_blank" className="underline">MDP-Développeur Freelance</a>. Tous droits
           réservés.
         </div>
       </div>
--- components/sections/QRcodeSection.tsx
+++ components/sections/QRcodeSection.tsx
@@ -5,7 +5,7 @@ const QRcodeSection = () => {
   return (
     <section>
       <Container className="py-0">
-        <div className="flex md:w-[800px] md:h-[500px] bg-primary mx-auto rounded-lg justify-center">
+        <div className="flex  bg-primary mx-auto md:mx-42 rounded-lg justify-center ">
           <div className="flex flex-col gap-4 md:gap-6 p-4 md:p-8">
             <h2 className="text-4xl font-playfair text-center mb-6">
               Vos photos comptent !
@@ -24,8 +24,45 @@ const QRcodeSection = () => {
                   src="https://placehold.co/200.png"
                   alt="QR Code pour partager les photos du mariage"
                   className="w-full h-full object-cover object-center border-6 border-white rounded-xl"
-                    fill
+                  fill
                 />
+              </div>
+            </div>
+            <div>
+              <p className="text-center">
+                Téléchargez l'application mobile <strong>WedShoots</strong> pour
+                accéder au QR code et partager vos photos facilement :
+              </p>
+              <div className="flex flex-col sm:flex-row gap-4 justify-center mt-6">
+                <a
+                  href="https://apps.apple.com/fr/app/wedshoots/id660256196"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition-transform duration-300 hover:scale-105"
+                >
+                  <Image
+                    src="/assets/badge-apple.svg"
+                    alt="Télécharger sur l'App Store"
+                    width={160}
+                    height={50}
+                    className="h-12 w-auto"
+                  />
+                </a>
+
+                <a
+                  href="https://play.google.com/store/apps/details?id=net.bodas.android.wedshoots&hl=fr"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition-transform duration-300 hover:scale-105"
+                >
+                  <Image
+                    src="/assets/badge-googleplay.svg"
+                    alt="Disponible sur Google Play"
+                    width={160}
+                    height={50}
+                    className="h-12 w-auto"
+                  />
+                </a>
               </div>
             </div>
           </div>
